6ec54a49b854fbcab73a2ad2d34ed7783b52e850
Deepakkrishna35 <89923452+Deepakkrishna35@users.noreply.github.com>
2022-11-30 17:35:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "5949707564:AAH0-E-Zrm_RW3L525a-q4SEmgfpaCqRKQI"
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
